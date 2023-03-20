1ba0d58beb16144499be6d42956c084026872f69
RomanistHere <romanisthere@pm.me>
2023-03-20 20:50:03 +0400

    fix: try to resolve issue

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -54,6 +54,7 @@ chrome.runtime.onInstalled.addListener(async (details) => {
 	})
 
 	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+		console.log(changeInfo);
 		if (changeInfo.status === 'complete') {
 			chrome.tabs.sendMessage(tabId, {
 				message: 'TabUpdated'
--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -134,6 +134,7 @@ const initMessTempl = () => {
 initMessTempl()
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	console.log(request);
 	if (request.message === 'TabUpdated') {
 		initMessTempl();
 	}
