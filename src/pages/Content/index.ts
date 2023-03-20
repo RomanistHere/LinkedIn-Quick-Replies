7ea464b10e35d6bf75a2c8ce7333c1b2c26ea1e0
RomanistHere <romanisthere@pm.me>
2023-03-20 21:00:39 +0400

    fix: remove log

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -134,7 +134,6 @@ const initMessTempl = () => {
 initMessTempl()
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	console.log(request);
 	if (request.message === 'TabUpdated') {
 		initMessTempl();
 	}
